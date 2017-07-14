3197c6927d1eed3cd855b2fb374e50f757643584
blbi <sskk1108@naver.com>
2017-07-14 16:59:06 -0400

    add filter

--- pcap1.c
+++ pcap1.c
@@ -16,6 +16,7 @@ void print_addr (u_char *not_used, const struct pcap_pkthdr *h, const u_char *p)
 {
     	int i;
     	struct ether_header *eh = (struct ether_header *)p;
+    
 	
     	printf("source mac address: ");
     	for (i=0; i<ETH_ALEN-2; i++)
@@ -52,11 +53,18 @@ void print_addr (u_char *not_used, const struct pcap_pkthdr *h, const u_char *p)
 	//	printf("ip destination address : %s\n", inet_ntoa(iph->ip_dst));
     	//	printf (" length = %d\n", h->len);
 //	}
-	}
+	
+	struct tcphdr *tcph = (struct tcphdr *)(p+sizeof(struct ether_header)+sizeof(struct ip));
+	
+	printf("Source Port : %d\n\n", ntohs(tcph->source));
+	printf("Destination Port : %d\n\n", ntohs(tcph->dest));
+}
 
 int main(int argc, char *argv[])
 {
+	struct bpf_program fp;
 	pcap_t  *pcd;
+	char filter_exp[]="port 80";
 
 	char errbuf[PCAP_ERRBUF_SIZE];
 	char *dev = pcap_lookupdev(errbuf);
@@ -65,6 +73,9 @@ int main(int argc, char *argv[])
         fprintf (stderr, "Device open failed\n");
         exit (1);
     	}
+	pcap_compile(pcd,&fp,filter_exp,0,net);
+	pcap_setfilter(pcd, &fp);
+
     	if (pcap_loop(pcd, MAXCOUNT, print_addr, NULL) < 0) {
         fprintf (stderr, "Error in pcap_loop()\n");
         exit (1);
